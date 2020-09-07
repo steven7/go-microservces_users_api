5924b5611434338ded75f30c9e2074d585bb0e88
steven7 <steven.kanceruk@gmail.com>
2020-09-07 16:53:40 -0400

    first commit

---
+++ controllers/users/users_controller.go
@@ -0,0 +1,14 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func GetUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "Implement this")
+}
+
+func CreateUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "Implement this")
+}
